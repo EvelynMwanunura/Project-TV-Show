8b33d333579bce776a084ca9bbc57d1538ea8853
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-20 12:38:52 +0200

    Edited the script js file and added type=module in the html document to enable function export and import

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 //You can edit ALL of the code here
+import { getAllEpisodes } from "./episodes.js";
+const rootElem = document.getElementById("root");
 let ulElement = document.createElement("ul");
-let bodyElement = document.getElementById("root");
-bodyElement.appendChild(ulElement);
+ulElement.style.listStyle = "none";
 
 function setup() {
   const allEpisodes = getAllEpisodes();
+
   makePageForEpisodes(allEpisodes);
   for (let episode of allEpisodes) {
     let liElement = document.createElement("li");
     liElement.innerHTML = `<h3>${episode.name}</h3> <p>Season: ${episode.season}, Episode: ${episode.number}</p>
     <img src = ${episode.image.medium}> <p>${episode.summary}</p>`;
+    rootElem.appendChild(ulElement);
     ulElement.appendChild(liElement);
   }
 }
 
-/*function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
+function makePageForEpisodes(episodeList) {
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}*/
+}
 
 window.onload = setup;
