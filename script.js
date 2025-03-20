8de2884bd193bcd5c4f2cbd86c775328b35c7858
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-20 11:34:24 +0200

    created variables and for loop to loop through the episodes

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 //You can edit ALL of the code here
+let ulElement = document.createElement("ul");
+let bodyElement = document.getElementById("root");
+bodyElement.appendChild(ulElement);
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  for (let episode of allEpisodes) {
+    let liElement = document.createElement("li");
+    liElement.innerHTML = `<h3>${episode.name}</h3> <p>Season: ${episode.season}, Episode: ${episode.number}</p>
+    <img src = ${episode.image.medium}> <p>${episode.summary}</p>`;
+    ulElement.appendChild(liElement);
+  }
 }
 
-function makePageForEpisodes(episodeList) {
+/*function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+}*/
 
 window.onload = setup;
