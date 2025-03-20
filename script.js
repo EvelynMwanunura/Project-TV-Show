34a336d1e3fe6d733206e81e628a3cd4440aea64
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-20 13:52:51 +0200

    changed the for loop to map method

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //You can edit ALL of the code here
 import { getAllEpisodes } from "./episodes.js";
+
 const rootElem = document.getElementById("root");
 let ulElement = document.createElement("ul");
 ulElement.style.listStyle = "none";
@@ -8,13 +9,14 @@ function setup() {
   const allEpisodes = getAllEpisodes();
 
   makePageForEpisodes(allEpisodes);
-  for (let episode of allEpisodes) {
+
+  allEpisodes.map((episode) => {
     let liElement = document.createElement("li");
     liElement.innerHTML = `<h3>${episode.name} - E${episode.season}S${episode.number}</h3> 
     <img src = ${episode.image.medium}> <p>${episode.summary}</p>`;
     rootElem.appendChild(ulElement);
     ulElement.appendChild(liElement);
-  }
+  });
 }
 
 function makePageForEpisodes(episodeList) {
