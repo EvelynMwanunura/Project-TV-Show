ac52e78731d7e6b5f67b3e8d585245556936e701
Faithy4444 <faithy4444@gmail.com>
2025-03-28 21:45:16 +0200

    rendering

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const rootElem = document.getElementById("root");
 let ulElement = document.createElement("ul");
+let searchInput = document.getElementById("search")
 ulElement.style.listStyle = "none";
 
 function setup() {
@@ -9,16 +10,32 @@ function setup() {
 
   makePageForEpisodes(allEpisodes);
 
+  searchInput.addEventListener('keyup', function(){
+    allEpisodes.searchTerm = searchInput.value
+
+    const filteredEpisodes = allEpisodes.filter(function(episode){
+      return episode.name.includes(episode.searchTerm) 
+    }
+    )
+  })
+}
+
+  function render(){
   allEpisodes.map((episode) => {
     let seasonNumber = episode.season.toString().padStart(2, "0");
     let episodeNumber = episode.number.toString().padStart(2, "0");
     let liElement = document.createElement("li");
     liElement.innerHTML = `<h3>${episode.name} - S${seasonNumber}E${episodeNumber}</h3> 
     <img src = ${episode.image.medium} alt ="episode Image"> <p>${episode.summary}</p>`;
-    rootElem.appendChild(ulElement);
+    
     ulElement.appendChild(liElement);
   });
+
+  rootElem.appendChild(ulElement);
 }
+
+render()
+
 
 function makePageForEpisodes(episodeList) {
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
